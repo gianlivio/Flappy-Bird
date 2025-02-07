b36f3d4ffc929a1fac02ce246dbe545a63eabeb2
Gianlivio Iemolo <gianlivioiemolo@gmail.com>
2025-02-07 17:25:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-        const canvas = document.getElementById("gameCanvas");
+         const canvas = document.getElementById("gameCanvas");
         const ctx = canvas.getContext("2d");
         canvas.width = 400;
         canvas.height = 600;
